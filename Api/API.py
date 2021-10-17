602823123cc803144c5aa7f494d86a0c4945782f
Akachi <anabantiakachi1@gmail.com>
2021-10-17 16:35:29 +0100

    Improved version before API for upload

--- Api/API.py
+++ Api/API.py
@@ -1,38 +1,41 @@
 #A function to download the file
 from __future__ import print_function
-import pickle
 import os.path
 import io
 import shutil
-import requests
 from mimetypes import MimeTypes
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+from google.oauth2.credentials import Credentials
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-creds = None
+def authenticate():
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
 
+        global service
+        service = build('drive', 'v3', credentials=creds)
 
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)  
-
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
-
-service = build('drive', 'v3', credentials=creds)
 
 def FileDownload(file_id, file_name):
     request = service.files().get_media(fileId=file_id)
@@ -77,7 +80,7 @@ def FileUpload(filepath):
         # Create a new file in the Drive storage
         file =service.files().create(
             body=file_metadata, media_body=media, fields='id').execute()
-        print("File Uploaded.")
+        #print("File Uploaded.")
 
     except Exception as ex:
         print("Can't upload file")
--- driveApp.py
+++ driveApp.py
@@ -8,9 +8,7 @@ from wtforms import SubmitField
 from flask_modals import Modal
 from flask_modals import render_template_modal
 from werkzeug.utils import secure_filename
-from wtforms.widgets.core import ListWidget, RadioInput
 from Api import API
-from flask import send_from_directory
 import shutil
 
 app = Flask(__name__)
@@ -21,23 +19,27 @@ app.config["UPLOAD_PATH"] = 'uploads'
 bootstrap=Bootstrap(app)
 modal=Modal(app)
 
+BOOTSTRAP_SERVE_LOCAL=True
+
 
 class FileForm(FlaskForm):
 	file=FileField(validators=[FileRequired()])
 	submit=SubmitField("Submit")
 
-class RadioForm(FlaskForm): #Still in Progress
-	pick=ListWidget('ul')
-
-
-
 @app.route('/', methods=['GET','POST'])
 def index():
+
+	#authenticate = API.authenticate()
+
 	form = FileForm()
 	flag = session.pop('flag', False)
+
 	if request.method=='POST':
+
 		uploaded_file = request.files['file']
+
 		filename = secure_filename(uploaded_file.filename)
+
 		if filename !='':
 			#trying to make the directory
 			os.mkdir(app.config["UPLOAD_PATH"])
@@ -47,6 +49,7 @@ def index():
 			uploaded_file.save(folder_path)
 
 			if form.validate_on_submit:
+				#API.authenticate()
 				#getting the list of the file in the folder
 				files=os.listdir(app.config['UPLOAD_PATH'])
 
@@ -65,12 +68,23 @@ def index():
 
 	modal = None if flag else 'modal-form'
 
-	return render_template_modal('index.html', form=form, modal=modal)
+	return render_template_modal('index.html', form=form, authenticate=True, modal=modal)
 
-
+	
 @app.route('/download', methods=['GET','POST'])
 def download():
-	items=API.GetFileList()
-	f_name = "name"
-	f_id = "id"
-	return render_template("download.html", items=items, f_name=f_name, f_id=f_id)
+	#items= #API.GetFileList()
+	
+	items=[{'id': '1srtAtR52s0g8xnMrkmuMzqs-en_L2M5n', 'name': 'cam18May2021151632.png'},
+    {'id': '1GGSF-zvXfWHs5Kcvgth31zirPcIf9VEE', 'name': 'Flask_Web_Development_Developing_Web_Applications_With_Python_by_Miguel_Grinberg_z-lib.org.pdf'}, 
+    {'id': '18XnZNpMIbaYdMDEPfnnA32RnDuxpkd0u', 'name': 'gweb.py'}, 
+    {'id': '1sRsS3-Is3lyBM3d6guANoTKF_lQB8JK3', 'name': 'drive-app.py'}, 
+    {'id': '1TffJSWw2sLlPMuCKJWcAWDPlyaNHIj1E', 'name': 'download-upload-drive.py'}, 
+    {'id': '1idAzrd7u0k-cq-Q60JoWcw-dV4aoGxpp', 'name': 'Copy of Welcome To Colaboratory'}, 
+    {'id': '1atsG_EyQSbney1d2hXW5j2KqN1X-Vk3nFuYNKOdI5Wk', 'name': 'COBWEB Manual'}, 
+    {'id': '14-aM0cUj7006GqzSxsKpq2Blv8o9MuDYk0iooWk9c28', 'name': 'Copy of COBWEB Manual'}, 
+    {'id': '1161YKuTmcMfvjxWBF74IQ6JWg_rAtqBN', 'name': 'COBWEB-Literature review'}, 
+    {'id': '1zA6Kj4Cx_oYAT1EycCqNow7gHYMdFLYg', 'name': 'me.jpg'}]
+
+
+	return render_template("download.html", items=items)
